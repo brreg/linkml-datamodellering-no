cfcf1bb134e04eefcda9f5d94f96c3180c59f572
AudunVindenesEggeBR <ave@brreg.no>
2026-08-09 22:53:28 +0200

    docs(mkdocs): flytter valideringsbadge etter versjonbadge.

--- mkdocs/lib/sections/badges.sh
+++ mkdocs/lib/sections/badges.sh
@@ -91,9 +91,9 @@ PYEOF
         echo "[![Utgiver](https://img.shields.io/badge/utgiver-${utgiver_encoded}-blue)]()"
     fi
     [ -n "$license" ] && echo "[![Lisens](https://img.shields.io/badge/NLOD-${license}-blue)]()"
-    echo "[![Validering](https://img.shields.io/badge/${policy_encoded}-${val_status_encoded}-${val_color})]()"
     echo "[![Status](https://img.shields.io/badge/status-${status_label}-${status_color})]()"
     echo "[![Versjon](https://img.shields.io/badge/versjon-${version}-blue)]()"
+    echo "[![Validering](https://img.shields.io/badge/${policy_encoded}-${val_status_encoded}-${val_color})]()"
     local endringsdato_encoded="${endringsdato//-/--}"
     [ -n "$endringsdato" ] && echo "[![Endringsdato](https://img.shields.io/badge/endringsdato-${endringsdato_encoded}-blue)]()"
     echo ""
